9e50c97c91b369f88a7ae0c9586cf3e96c0299c7
JagTheFriend <JagTheFriend12@gmail.com>
2026-03-18 22:15:29 +0530

    Setup logger

--- server/server.go
+++ server/server.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"common"
+	"context"
+	"log/slog"
 	"net/http"
+	"os"
 	"server/routes"
 	"time"
 
@@ -20,6 +23,28 @@ func StartServer() {
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.ContextTimeout(time.Second * 5))
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c *echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+				)
+			} else {
+				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+				)
+			}
+			return nil
+		},
+	}))
 
 	groupedRoute := e.Group("/api/v1")
 	websocketHandler := routes.NewWebSocketHandler(groupedRoute)
